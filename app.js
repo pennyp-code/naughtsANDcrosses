899633cea8f5178125c7666f2b835f1bf1d3be39
pennyp-code <129231950+pennyp-code@users.noreply.github.com>
2023-10-03 15:25:27 +1100

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const startCells = [
 function createBoard(){
     startCells.forEach((cell, index) =>{
         const cellElement = document.createElement('div')
-    cellElement.classList
+    cellElement.classList.add('square')
+    gameBoard.append(cellElement)
     })
 }
+
+createBoard();
